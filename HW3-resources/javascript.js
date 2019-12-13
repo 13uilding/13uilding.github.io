d90d369441660e44604c1f401c764d2b58b63f98
Fhor <hereisdanbrown@gmail.com>
2019-12-12 19:38:34 -0700

    Changed the appearance and added buttons corresponding to the different parameters

--- HW3-resources/javascript.js
+++ HW3-resources/javascript.js
@@ -1,4 +1,4 @@
-// Functions
+// Functions ===========================================================================
 function randomInt(n){
     result = Math.floor(Math.random() * n);
     return result
@@ -15,85 +15,105 @@ function charInArray(arrMain, arrTest){
 };
 // The Join method does this better, but I already wrote it, so whatever.
 const toString = (letters, letter) => letters + letter;
-// Arrays
-let upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let specialArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-'];
-let numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// While loop condition & While loop
-let questionTime = true;
-while (questionTime){
-    var userLength = prompt("What length do you want for your password (between 8-128 characters)");
-    // Determines if the user input is the correct length
-    if ((userLength < 8) || (userLength > 128)){
-        alert("Please enter another number between 8-128.")
-        continue
-    };
-    var userSpecialCase = confirm("Do you want any special characters?");
-    var userLowerCase = confirm("Do you want lowercase characters?");
-    var userUpperCase = confirm("Do you want uppercase characters?");
-    var userNumberCase = confirm("Do you want numbers?")
-    console.log(userSpecialCase)
-    // If any of the criteria is accepted, we break the while loop
-    if (userSpecialCase || userLowerCase || userUpperCase || userNumberCase){
-        questionTime = false;
-    } else { alert("Please accept one of the given criteria.")};
-};
-
-// Building my potential character array for the password
-let charArr = [];
-if (userSpecialCase){ charArr = [...specialArr]};
-if (userUpperCase){ charArr = [...charArr, ...upperArr]};
-if (userLowerCase){ charArr = [...charArr, ...lowerArr]};
-if (userNumberCase){ charArr = [...charArr, ...numberArr]};
-
-let notContainsCriteria = true;
-while(notContainsCriteria) {
-    var result = "";
-    lenCharArr = charArr.length;
-    passwordArr = [];
-    for (let i = 0; i < userLength; i++){
-        newCharIndex = randomInt(lenCharArr);
-        // console.log("New char index is " + newCharIndex);
-        let newChar = charArr[newCharIndex];
-        passwordArr[i] = newChar;
-    };
-    // Makes sure that we have every type FIX THIS
-    if (userSpecialCase) {
-        let specialCharTest = charInArray(passwordArr, specialArr);
-        if (specialCharTest === false){
-            continue;
-        };
-    };
-    if (userNumberCase){
-        let numberCharTest = charInArray(passwordArr, numberArr);
-        if (numberCharTest === false){
-            continue;
-        };
-    };
-    if (userUpperCase){
-        let upperCharTest = charInArray(passwordArr, upperArr); 
-        if (upperCharTest === false){
-            continue;
-        }  
-    };
-    if (userLowerCase){
-        let lowerCharTest = charInArray(passwordArr, lowerArr);
-        if (lowerCharTest === false){
-            continue;
-        };
-    };
-    notContainsCriteria = false;
-};
-
 // Writing to the page!
 function newContent() {
     document.open();
     document.write("<h1>Out with the old - in with the new!</h1>");
     document.close();
 }
-result = passwordArr.reduce(toString);
-alert(result);
+
+// Control
+var generateBtn = document.getElementById("generateBtn");
+var copyBtn = document.getElementById("copyBtn");
+var eraseBtn = document.getElementById("eraseBtn");
+var passwordText = document.getElementById("passwordText");
+var length
+
+generateBtn.addEventListener("click", function(){
+    console.log("generating");
+});
+copyBtn.addEventListener("click", function(){
+    console.log("copying");
+});
+eraseBtn.addEventListener("click", function(){
+    console.log("erasing");
+});
+
+// Arrays ===========================================================================
+let upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let specialArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-'];
+let numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// // While loop which controls the prompt and confirms that the user provides
+// let questionTime = true;
+// while (questionTime){
+//     var userLength = prompt("What length do you want for your password (between 8-128 characters)");
+//     // Determines if the user input is the correct length
+//     if ((userLength < 8) || (userLength > 128)){
+//         alert("Please enter another number between 8-128.")
+//         continue
+//     };
+//     var userSpecialCase = confirm("Do you want any special characters?");
+//     var userLowerCase = confirm("Do you want lowercase characters?");
+//     var userUpperCase = confirm("Do you want uppercase characters?");
+//     var userNumberCase = confirm("Do you want numbers?")
+//     console.log(userSpecialCase)
+//     // If any of the criteria is accepted, we break the while loop
+//     if (userSpecialCase || userLowerCase || userUpperCase || userNumberCase){
+//         questionTime = false;
+//     } else { alert("Please accept one of the given criteria.")};
+// };
+
+// // Building my character array for the password
+// let charArr = [];
+// if (userSpecialCase){ charArr = [...specialArr]};
+// if (userUpperCase){ charArr = [...charArr, ...upperArr]};
+// if (userLowerCase){ charArr = [...charArr, ...lowerArr]};
+// if (userNumberCase){ charArr = [...charArr, ...numberArr]};
+
+// // While loop which creates the password and then tests to verify it contains all the given characters
+// let notContainsCriteria = true;
+// while(notContainsCriteria) {
+//     var result = "";
+//     lenCharArr = charArr.length;
+//     passwordArr = [];
+//     for (let i = 0; i < userLength; i++){
+//         newCharIndex = randomInt(lenCharArr);
+//         // console.log("New char index is " + newCharIndex);
+//         let newChar = charArr[newCharIndex];
+//         passwordArr[i] = newChar;
+//     };
+//     // Makes sure that we have every type FIX THIS
+//     if (userNumberCase){
+//         let numberCharTest = charInArray(passwordArr, numberArr);
+//         if (numberCharTest === false){
+//             continue;
+//         };
+//     };
+//     if (userSpecialCase) {
+//         let specialCharTest = charInArray(passwordArr, specialArr);
+//         if (specialCharTest === false){
+//             continue;
+//         };
+//     };
+//     if (userUpperCase){
+//         let upperCharTest = charInArray(passwordArr, upperArr); 
+//         if (upperCharTest === false){
+//             continue;
+//         }  
+//     };
+//     if (userLowerCase){
+//         let lowerCharTest = charInArray(passwordArr, lowerArr);
+//         if (lowerCharTest === false){
+//             continue;
+//         };
+//     };
+//     notContainsCriteria = false;
+// };
+
+// result = passwordArr.reduce(toString);
+
+let result = "YODA"
 document.write(result);
 
