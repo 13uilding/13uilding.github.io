c48a8f4dbaf849bb0e33b679fe07babc2c78dc46
deep <hereisdanbrown@gmail.com>
2019-12-16 22:02:41 -0700

    Assigned a single instance of what question will be displayed along with the corresponding answers.

--- HWs/HW4/javascript.js
+++ HWs/HW4/javascript.js
@@ -1,22 +1,58 @@
+// Variables
+  // Buttons
 var highScoreBtn = document.querySelector("button");
 var startBtn = document.querySelector("h1");
+  // Time
 var timeEl = document.getElementById("timer");
-var questionWrapper = document.getElementById("main");
 var secondsLeft = 60;
+  // Quiz
+var quizQuestions = document.getElementById("questionWrapper");
+var quizQ = quizQuestions.children[0];
+var quizA0 = document.getElementById("A0")
+var quizA1 = document.getElementById("A1")
+var quizA2 = document.getElementById("A2")
+var quizA3 = document.getElementById("A3")
+const qArr = ["correct", "wrong1", "wrong2", "wrong3"];
+  // Question 1
+var q1 = {
+  question: "Which method doesn't work on an array?", 
+  correct: "The .split() method.",
+  wrong1: "The .slice() method.",
+  wrong2: "The .splice() method.",
+  wrong3: "The .join() method."
+  };
+  // Score
 var highScores = [];
 var score;
-const qArr = ["correct", "wrong1", "wrong2", "wrong3"];
 
+
+// Buttons
 highScoreBtn.addEventListener("click", function(){
     console.log("you clicked me");
     score ++; //Remove this later
 });
 
 startBtn.addEventListener("click", function(){
-    console.log("Timer Started");
-    setTime();
+  setTime();
+  runQuiz();
+  console.log("Timer Started");
 });
 
+
+// Functions
+// Quiz
+function runQuiz(){
+  quizQuestions.setAttribute("style", "display: block");
+  // Create another instance of randomIndexArray on the questions
+  // Then I loop through the questions var arr = [q1, q2, q3, ...]
+  var options = randomIndexArray(qArr);
+  quizQ.textContent = q1.question;
+  quizA0.textContent = q1[options[0]];
+  quizA1.textContent = q1[options[1]];
+  quizA2.textContent = q1[options[2]];
+  quizA3.textContent = q1[options[3]];
+
+};
 
 
 // Timer
@@ -28,7 +64,8 @@ function setTime() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      logHighScore();
+      quizQuestions.setAttribute("style", "display: none");
+      logScore();
       timeEl.textContent = "Timer: "
       secondsLeft = 60;
     }
@@ -36,8 +73,24 @@ function setTime() {
   }, 1000);
 }
 
+// randomize the questions using this array
+function randomIndexArray(arr){
+  var qIndex = [];
+  while ( qIndex.length !== arr.length ){
+    var index = Math.floor(Math.random() * arr.length);
+    if ( qIndex.includes(index) ) {
+      continue;
+    } else qIndex.push(index);
+  };
+  var options = []
+  for (let i = 0; i < qIndex.length; i++){
+    options.push(arr[qIndex[i]]);
+  };
+  return options;
+};
+
 // Going to need to change this to write to local storage instead
-function logHighScore() {
+function logScore() {
   console.log(score.textContent);
 
   console.log("display input form for your score");
@@ -48,15 +101,3 @@ function logHighScore() {
 //   questionWrapper.appendChild(imgEl);
 
 }
-
-// randomize the questions using this array
-function randomIndexArray(){
-  var qIndexArr = [];
-  while ( qIndexArr.length !== qArr.length ){
-    var index = Math.floor(Math.random() * qArr.length);
-    if ( qIndexArr.includes(index) ) {
-      continue;
-    } else qIndexArr.push(index);
-  };
-  return qIndexArr;
-};
